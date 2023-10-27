9ecfaaceb8bac6bff6d6ab122b0934cd16d02fcd
buck0451 <buck0451@yandex.com>
2023-10-27 17:37:14 +0300

    NIO

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,86 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (
+            RandomAccessFile raFile = new RandomAccessFile(file, "r");
+            FileChannel inChannel = raFile.getChannel();
+            ) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (inChannel.read(buffer) > 0) {
+                buffer.flip();
+                StringBuilder builder = new StringBuilder();
+                Flag flag = Flag.NAME;
+                for (int i = 0; i < buffer.limit(); i++) {
+                    char ch = (char) buffer.get();
+
+                    if (ch == '\n') {
+                        switch (flag) {
+                            case NAME:
+                                profile.setName(builder.toString());
+                                builder.delete(0, builder.length());
+                                break;
+                            case AGE:
+                                profile.setAge(Integer.parseInt(builder.toString()));
+                                builder.delete(0, builder.length());
+                                break;
+                            case EMAIL:
+                                profile.setEmail(builder.toString());
+                                builder.delete(0, builder.length());
+                                break;
+                            case PHONE:
+                                profile.setPhone(Long.parseLong(builder.toString()));
+                                builder.delete(0, builder.length());
+                                break;
+                        }
+                    } else {
+                        builder.append(ch);
+                        switch (builder.toString()) {
+                            case "Name: ":
+                                builder.delete(0, builder.length());
+                                flag = Flag.NAME;
+                                break;
+                            case "Age: ":
+                                builder.delete(0, builder.length());
+                                flag = Flag.AGE;
+                                break;
+                            case "Email: ":
+                                builder.delete(0, builder.length());
+                                flag = Flag.EMAIL;
+                                break;
+                            case "Phone: ":
+                                builder.delete(0, builder.length());
+                                flag = Flag.PHONE;
+                                break;
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
     }
+
+    private enum Flag {
+        NAME,
+        AGE,
+        EMAIL,
+        PHONE
+    }
+
+
 }
